6ae865c3644691f07a0ffc8111d227d5909e8d01
unknown <anthonyjosdav007@gmail.com>
2024-03-09 11:50:34 -0400

    segundo astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Anthovalen.github.io',/* aqui colocarias el nombre de tu cuenta*/
+    base: '/astro',/* aqui colocarias el nombre de tu repositorio*/
+    build: {
+    assets: 'astro' /// muy importante
+    }
+    });
